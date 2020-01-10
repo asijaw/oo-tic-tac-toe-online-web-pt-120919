40db4e0ea7ddece97d41956677eb62e3144c5f50
Asija Watson <asija.w@yahoo.com>
2020-01-10 06:18:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,8 +115,6 @@ class TicTacToe
   def play 
      turn until over?
      won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
-    # puts("Would you like to play again? (Y or N)")
-    # gets.strip == "Y" ? play : puts("Goodbye!")
   end
 end
     
